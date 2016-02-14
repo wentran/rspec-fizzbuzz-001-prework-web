30e603a851de62f36a2a19cb9c7234cf5fe4f7de
Wen Tran <wentran@Wen-Trans-MacBook-Pro.local>
2016-02-14 14:25:50 -0500

    Done.

---
+++ fizzbuzz.rb
@@ -0,0 +1,15 @@
+def fizzbuzz(num)
+  if num % 3 == 0 && num % 5 == 0
+   return "FizzBuzz"
+    num % 3 == 0 
+   "Fizz"
+  elsif num % 5 == 0 
+   "Buzz"  
+  elsif num % 3 == 0 
+   "Fizz"
+  else 
+    nil
+  end
+end     
+       
+ 
